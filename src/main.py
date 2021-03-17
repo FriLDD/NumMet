91bc880707dbae9fda444fe85b649ed5e6ef0cba
FriLDD <dotpointen@gmail.com>
2021-03-17 21:37:48 +0300

    A prototype of the parent class and the descendant classes was created.

---
+++ src/main.py
@@ -0,0 +1,29 @@
+import math
+
+
+class NumMet:
+    """The parent class whose task is to analyze the input string"""
+    def __init__(self, equation_1, equation_2):
+        pass
+
+    def parse_equation(self, eq):
+        """The function analyzes the input strings for further
+            mathematical processing of the equations"""
+        pass
+    pass
+
+
+class NewtonMethod(NumMet):
+    """Solving a system of nonlinear equations by the Newton method"""
+    def __init__(self, equation_1, equation_2):
+        super().__init__(equation_1, equation_2)
+        pass
+    pass
+
+
+class RelaxationMethod(NumMet):
+    """Solution of a system of nonlinear equations by the relaxation method"""
+    def __init__(self, equation_1, equation_2):
+        super().__init__(equation_1, equation_2)
+        pass
+    pass
