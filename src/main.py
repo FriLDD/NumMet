8c89714dff0c7ee2908e4f6e6c3c810f98b75719
FriLDD <dotpointen@gmail.com>
2021-03-30 11:45:52 +0300

    First task complete

--- src/main.py
+++ src/main.py
@@ -1,29 +1,48 @@
 import math
+import numpy
+from matplotlib import pyplot as plt
 
 
-class NumMet:
-    """The parent class whose task is to analyze the input string"""
-    def __init__(self, equation_1, equation_2):
-        pass
-
-    def parse_equation(self, eq):
-        """The function analyzes the input strings for further
-            mathematical processing of the equations"""
-        pass
-    pass
+def func_x(y):
+    x = 0.7 - math.cos(y - 1)
+    return x
 
 
-class NewtonMethod(NumMet):
-    """Solving a system of nonlinear equations by the Newton method"""
-    def __init__(self, equation_1, equation_2):
-        super().__init__(equation_1, equation_2)
-        pass
-    pass
+def func_y(x):
+    y = 1 - math.sin(x) / 2
+    return y
 
 
-class RelaxationMethod(NumMet):
-    """Solution of a system of nonlinear equations by the relaxation method"""
-    def __init__(self, equation_1, equation_2):
-        super().__init__(equation_1, equation_2)
-        pass
-    pass
+def draw_graph(a, b, n):
+    x_arr, y_arr = [], []
+    dot_count = numpy.linspace(a, b, n)
+    for i in dot_count:
+        x_arr.append(func_x(i))
+        y_arr.append(func_y(i))
+    plt.plot(x_arr, dot_count, dot_count, y_arr)
+    plt.grid()
+    plt.show()
+
+
+def system_solution(accuracy, x_0, y_0):
+    x = func_x(y_0)
+    y = func_y(x_0)
+    k = 1
+    while (abs(x - x_0) > accuracy) or (abs(y - y_0) > accuracy):
+        x_0 = x
+        y_0 = y
+        x = func_x(y_0)
+        y = func_y(x_0)
+        k += 1
+    print(f"Количество итераций: {k}\n"
+          f"Решение: x = {x}, y = {y}\n"
+          f"deltaX = {x - x_0}\n"
+          f"deltaY = {y - y_0}\n"
+          f"f1 = {func_x(y) - x}\n"
+          f"f2 = {func_y(x) - y}")
+
+
+draw_graph(-5, 5, 150)
+x_graph = 0
+y_graph = 1
+system_solution(0.01, x_graph, y_graph)
